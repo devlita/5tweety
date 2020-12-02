5f2b57478bc6c3f8f1b43a9935187f3f278e6e6f
devlita <karmelitadevni@gmail.com>
2020-12-02 15:38:29 +0800

    design the timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,17 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/40"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-2">Jane Doe</h5>
+
+        <p class="text-sm">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        </p>
+    </div>
+</div>
