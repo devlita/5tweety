94180bcac299537537fccb8e22eaf006ea33bc26
devlita <karmelitadevni@gmail.com>
2020-12-10 15:27:59 +0800

    build a following list

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,24 +1,24 @@
-<div class="border border-blue-400 rounded-lg py-6 px-8 mb-8">
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
     <form method="POST" action="/tweets">
         @csrf
 
-            <textarea 
-                name="body" 
-                class="w-full"
-                placeholder="what's up doc?"
-            ></textarea>
-        
+        <textarea
+            name="body"
+            class="w-full"
+            placeholder="What's up doc?"
+        ></textarea>
+
         <hr class="my-4">
 
         <footer class="flex justify-between">
-            <img 
+            <img
                 src="{{ auth()->user()->avatar }}"
                 alt="your avatar"
                 class="rounded-full mr-2"
             >
 
-            <button 
-                type="submit" 
+            <button
+                type="submit"
                 class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"
             >
                 Tweet-a-roo!
@@ -26,7 +26,7 @@
         </footer>
     </form>
 
-    @error('body')
-        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
-    @enderror
+    @if ($errors->has('body'))
+        <p class="text-red-500 text-sm mt-2">{{ $errors->first('body') }}</p>
+    @endif
 </div>
